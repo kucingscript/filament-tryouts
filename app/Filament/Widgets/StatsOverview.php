1c9960d61fd8176d33c1408d465600456074d748
Ar Rasyid Sarifullah <rasyidzkun@gmail.com>
2024-08-27 07:48:26 +0700

    fix: stats overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -7,13 +7,20 @@ use App\Models\Question;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Spatie\Permission\Models\Role;
 
 class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Students', User::role('siswa')->count()),
+            Stat::make('Total Students', function () {
+                if (Role::where('name', 'siswa')->exists()) {
+                    return User::role('siswa')->count();
+                } else {
+                    return '0';
+                }
+            }),
             Stat::make('Question Packages', Package::all()->count()),
             Stat::make('Total Questions', Question::all()->count()),
         ];
